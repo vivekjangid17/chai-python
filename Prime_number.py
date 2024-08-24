5eb015c036cf9c77ffb61d0e293a51988eee2ca6
vivekjangid17 <vj833930@gmail.com>
2024-08-24 11:00:38 +0530

    modification in the code

--- Prime_number.py
+++ Prime_number.py
@@ -4,14 +4,13 @@
 
 n = int(input("Enter a number: "))
 
-
-for i in range(2,n):
-    if n%i==0:
-        print(f"{n} is not a prime number.")
-        break
+if n <= 1:
+    print(f"{n} is not prime")
+else:
+    for i in range(2,n):
+        if n%i==0:
+            print(f"{n} is not a prime number.")
+            break
     else:
         print(f"{n} is a prime number")
-        
-    
-    
-    
+            
